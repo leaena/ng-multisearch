b5e9f0ea44f6818b633764617b5449379e31a66f
Randall Leeds <randall@bleeds.info>
2014-02-27 13:54:16 -0800

    Refactor ng-model and add facets component API

--- multisearch.js
+++ multisearch.js
@@ -1,40 +1,40 @@
 angular.module("multiSearch", [])
-.controller("multiSearch", function($scope, $rootScope){
+.controller("multiSearch", function($scope){
   $scope.search = '';
   $scope.results = '';
   $scope.$watch('search', function(newValue){
     var terms = null;
     var parts = newValue.match(/(\w+(?::|$)(?: ?\b\w+\b(?!:))*)+/g);
-    if (parts) terms = parts.map(function (p) {
+    if (parts) $scope.model = parts.map(function (p) {
       return p.split(':').map(function (s) {
         return s.trim();
       });
     });
-    $scope.terms = terms;
-    if(terms) $scope.parse();
   });
-  $scope.parse = function(){
-    var length = $scope.terms.length;
-    var last = $scope.terms[length-1];
-    if(last[1]){
-      $scope.stem = last[1];
-      $scope.completions = $rootScope.data[last[0]];
-    } else {
-      $scope.stem = last[0];
-      $scope.completions = Object.keys($rootScope.data);
-    }
-  }
 })
 .directive('multisearch', function(){
   return {
     link: function (scope, elem, attrs, controller) {
+      controller.$render = function () {
+        if (!scope.model) return;
+        var length = scope.model.length;
+        var last = scope.model[length-1];
+        if(last[1]){
+          scope.stem = last[1];
+          scope.completions = scope.facets()[last[0]];
+        } else {
+          scope.stem = last[0];
+          scope.completions = Object.keys(scope.facets());
+        }
+      };
     },
     controller: 'multiSearch',
     templateUrl: 'search.html',
     require: 'ngModel',
     restrict: 'E',
     scope: {
-      categories: '=ngModel'
+      facets: '&',
+      model: '=ngModel'
     }
   }
 })
