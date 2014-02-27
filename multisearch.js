49b549c78b0fcc677a0d67728fb7ab8557a2563a
Lindsey Smith <lindseyengineer@gmail.com>
2014-02-27 13:03:41 -0800

    start of search filter

--- multisearch.js
+++ multisearch.js
@@ -1,16 +1,32 @@
 angular.module("multiSearch", [])
 .controller("multiSearch", function($scope){
   $scope.search = '';
-  $scope.results = [];
-  $scope.parts = ["Sta", "New York"];
+  $scope.results = '';
   $scope.$watch('search', function(newValue){
     var terms = null;
     var parts = newValue.match(/(\w+(?::|$)(?: ?\b\w+\b(?!:))*)+/g);
     if (parts) terms = parts.map(function (p) { return p.split(':'); });
     $scope.terms = terms;
+    if(terms) $scope.parse();
   });
-  $scope.parse = function(){
+  $scope.searchFilter = function(){
+    if($scope.value){
+      return function(item){
+        return $scope.results === item
+      }
+    }
 
+  }
+  $scope.parse = function(){
+    var length = $scope.terms.length;
+    var last = $scope.terms[length-1];
+    if(last[1]){
+      $scope.value = true;
+      $scope.results = last[1];
+    } else {
+      $scope.value = false;
+      $scope.results = last[0];
+    }
   }
 })
 .directive('multisearch', function(){
