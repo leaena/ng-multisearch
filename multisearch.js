ea76e1a858127ebb41688e579bc1d44692bebb8e
Randall Leeds <randall@bleeds.info>
2014-02-27 12:37:35 -0800

    trim term components

--- multisearch.js
+++ multisearch.js
@@ -6,7 +6,11 @@ angular.module("multiSearch", [])
   $scope.$watch('search', function(newValue){
     var terms = null;
     var parts = newValue.match(/(\w+(?::|$)(?: ?\b\w+\b(?!:))*)+/g);
-    if (parts) terms = parts.map(function (p) { return p.split(':'); });
+    if (parts) terms = parts.map(function (p) {
+      return p.split(':').map(function (s) {
+        return s.trim()
+      });
+    });
     $scope.terms = terms;
   });
   $scope.parse = function(){
