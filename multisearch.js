a82e6e654aeedf1a97e9fbf28cb34344d9628808
Randall Leeds <randall@bleeds.info>
2014-02-27 12:18:03 -0800

    Basic key:value regexp

--- multisearch.js
+++ multisearch.js
@@ -2,7 +2,10 @@ angular.module("multiSearch", [])
 .controller("multiSearch", function($scope){
   $scope.search = '';
   $scope.$watch('search', function(newValue){
-
+    var terms = null;
+    var parts = newValue.match(/(\w+(?::|$)(?:\w+(?!:))*)+/g);
+    if (parts) terms = parts.map(function (p) { return p.split(':'); });
+    $scope.terms = terms;
   });
 })
 .directive('multisearch', function(){
